2d3fe5368539e3c3cbce9bb401dc140165f1f0d1
Tung Pham Thanh <tung.ph.th@gmail.com>
2014-02-11 14:53:19 +0100

    update FB appId

--- app/config/berlin01/facebook.php
+++ app/config/berlin01/facebook.php
@@ -2,13 +2,13 @@
 
 return array
 (
-	'appId' 	=> '272352426248496',
-	'appSecret'	=> 'd10e206e0272e761811ee21a15d64c8f',
+	'appId' 	=> '236005876495095',
+	'appSecret'	=> '90abe678ae78c24b6558a26867e0dfab',
 	'hostname'  => $_SERVER['HTTP_HOST'],
 	'dirname'   => dirname($_SERVER['PHP_SELF']).'/',
-	'appUrl'    => 'https://www.facebook.com/tungse/app_272352426248496?',
-	'canvas' 	=> 'https://apps.facebook.com/tung_laravel_default',
+	'appUrl'    => 'http://www.facebook.com/tungse/app_236005876495095?',
+	'canvas' 	=> 'https://apps.facebook.com/tungsedefault',
 	'pageId'	=> '131592306961101',
-	'namespace' => 'tung_laravel_default',
+	'namespace' => 'tungsedefault',
 	'channel' 	=> '',
 );
