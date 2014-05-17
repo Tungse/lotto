2f0fc9a00b6d24b666fb57b7f9643b9ffebd918c
Tung Pham Thanh <tung.ph.th@gmail.com>
2014-05-17 12:39:15 +0200

    initial commit

---
+++ app/views/index/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de" xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://ogp.me/ns#">
+	<head>
+		<title></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/_common.css" />
+		<link rel="stylesheet" type="text/css" href="css/index/index.css" />
+		<script>
+			var setting = 
+			{
+				
+			}
+		</script>
+		<script language="Javascript" type="text/Javascript" src="js/library/jquery.js"></script>
+		<script language="Javascript" type="text/Javascript" src="js/_common.js"></script>
+		<script language="Javascript" type="text/Javascript" src="js/index/index.js"></script>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="index-holder">
+				
+			</div>
+		</div>
+	</body>
+</html>
